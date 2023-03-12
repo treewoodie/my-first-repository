4dc66fe4adab988f1ca713129f65116b6dd9b188
김지수 <wltnvhs1@gmail.com>
2023-03-12 22:41:11 +0900

    -first

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
 console.log(agoraStatesDiscussions);
 
+
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
 const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
@@ -14,12 +15,67 @@ const convertToDiscussion = (obj) => {
   discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
+  // li에 걸려있는 링크 연결
+const avatarImg = document.createElement('img');
+avatarImg.className = "discussion__avatar--image";
+avatarImg.src = obj.avatarUrl;
+avatarImg.alt = `avatar of ${obj.author}`
+avatarWrapper.append(avatarImg)
 
+const contentTitle = document.createElement('h3');
+contentTitle.className = "discussion__title";
+const titleAnchor = document.createElement('a');
+titleAnchor.href =obj.url;
+titleAnchor.textContent = obj.title;
+contentTitle.append(titleAnchor)
+const contentInfo = document.createElement('div');
+contentInfo.className = "discussion__information";
+contentInfo.textContent = `${obj.author} / ${new Date(obj.createdAt).toLocaleString()}`
+discussionContent.append(contentTitle,contentInfo);
 
+const checked = document.createElement('p');
+checked.textContent = obj.answer ? '✔︎' : '✘';
+discussionAnswered.append(checked)
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
+
+
+const form = document.querySelector('form.form');
+const title = document.querySelector('input#title');
+const author = document.querySelector('input#name');
+const story = document.querySelector('textarea#story');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  const newDiscussion = {
+    id: "unique value",
+    createdAt: new Date(),
+    title: title.value,
+    url: "https://github.com/codestates-seb/agora-states-fe/discussions/45",
+    author: author.value,
+    answer: {
+      id: "DC_kwDOHOApLM4AKg6M",
+      createdAt: "2022-05-16T02:09:52Z",
+      url: "https://github.com/codestates-seb/agora-states-fe/discussions/45#discussioncomment-2756236",
+      author: "Kingsenal",
+      bodyHTML:
+        '<p dir="auto">안녕하세요. <a class="user-mention notranslate" data-hovercard-type="user" data-hovercard-url="/users/dubipy/hovercard" data-octo-click="hovercard-link-click" data-octo-dimensions="link_type:self" href="https://github.com/dubipy">@dubipy</a> 님!<br>\n코드스테이츠 교육 엔지니어 권준혁 입니다. <g-emoji class="g-emoji" alias="raised_hands" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f64c.png">🙌</g-emoji></p>\n<p dir="auto">질문 주신 내용은 노드 환경이 구성되어 있지 않기 때문에 발생되는 문제로 확인됩니다.</p>\n<p dir="auto"><code class="notranslate">brew unlink node &amp;&amp; brew link node</code></p>\n<p dir="auto">노드를 연결해 보시고 안된다면</p>\n<p dir="auto"><code class="notranslate">brew link --overwrite node</code></p>\n<p dir="auto">이 명령어를 그 다음에도 안된다면 접근권한 문제일 가능성이 큽니다.</p>\n<p dir="auto"><code class="notranslate">$ sudo chmod 776 /usr/local/lib</code> 접근 권한 변경 후<br>\n<code class="notranslate">$ brew link --overwrite node</code> 다시 연결을 해보세요 !</p>\n<p dir="auto">그럼에도 안된다면 다시 한 번 더 질문을 남겨주세요 !</p>\n<p dir="auto">답변이 되셨다면 내용을 간략하게 정리해서 코멘트를 남기고 answered를 마크해주세요 <g-emoji class="g-emoji" alias="white_check_mark" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2705.png">✅</g-emoji><br>\n감사합니다.<g-emoji class="g-emoji" alias="rocket" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/1f680.png">🚀</g-emoji><br>\n코드스테이츠 교육 엔지니어 권준혁</p>',
+      avatarUrl: "https://avatars.githubusercontent.com/u/79903256?s=64&v=4",
+    },
+    bodyHTML: story.value,
+    avatarUrl:
+      "https://avatars.githubusercontent.com/u/97888923?s=64&u=12b18768cdeebcf358b70051283a3ef57be6a20f&v=4",
+  }
+  ul.prepend(convertToDiscussion(newDiscussion)) 
+  title.value = "";
+  author.value = "";
+  story.value = ""
+})
+
+
+
 
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
